fba52cfdb0aef06fede3a5627c329a8e2c6dab29
Jai Pal Singh <jai.pal.2013@gmail.com>
2018-03-30 11:32:51 +0530

    Initial Commit

---
+++ app/src/main/java/com/android_developer/jaipal/sim/MainActivity.java
@@ -0,0 +1,92 @@
+package com.android_developer.jaipal.sim;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private String[] division = { "-", "JP", "JU", "AII", "BKN", };
+    private EditText dateOfInspectionEtxt;
+    private DatePickerDialog datePicker;
+    private SimpleDateFormat dateFormatter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner divisionspin = (Spinner) findViewById(R.id.divisionSpinner);
+        divisionspin.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        //Creating the ArrayAdapter instance
+        ArrayAdapter divisionAA = new ArrayAdapter(this,android.R.layout.simple_spinner_item,division);
+        divisionAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Setting the ArrayAdapter data on the Spinner
+        divisionspin.setAdapter(divisionAA);
+
+        //Date formatter
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        findViewsById();
+
+        setDateTimeField();
+    }
+
+    private void findViewsById() {
+        dateOfInspectionEtxt = (EditText) findViewById(R.id.dateOfInspectionEditText);
+        dateOfInspectionEtxt.setInputType(InputType.TYPE_NULL);
+        dateOfInspectionEtxt.requestFocus();
+    }
+
+    private void setDateTimeField() {
+        dateOfInspectionEtxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.show();
+            }
+        });
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dateOfInspectionEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
